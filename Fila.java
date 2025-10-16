ab8e2df733a6fba5bf8412592fec2dce522f1996
Adryan Costa Silva <adryanbrabuu@gmail.com>
2025-10-16 18:29:36 -0300

    Finish Fila.java

--- Fila.java
+++ Fila.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Fila {
+    private NodeCliente primeiro;
+    private NodeCliente ultimo;
+
+    public Fila(){
+        this.primeiro = null;
+        this.ultimo = null;
+    }
+
+    public void adicionarCliente(ElementoCliente cliente){
+        NodeCliente novoCLiente = new NodeCliente();
+        novoCLiente.setInformation(cliente);
+        novoCLiente.setNextNode(null);
+
+        if (vazia()){
+            primeiro = novoCLiente;
+            ultimo = novoCLiente;
+        } else{
+            ultimo.setNextNode(novoCLiente);
+            ultimo = novoCLiente;
+        }
+    }
+
+    public void atenderCliente(){
+        if (vazia()){
+            System.out.println("Nenhum cliente na fila!");
+            return;
+        }
+
+        primeiro = primeiro.getNextNode();
+
+        if (primeiro == null){
+            ultimo = null;
+        }
+    }
+
+    public void imprimirClientes(){
+        NodeCliente atual = primeiro;
+        int count = 0;
+        while (atual != null){
+            count ++;
+            System.out.println("["+count+"] Cliente - ID: "+atual.getInformation().getId()+ " | Nome: "+atual.getInformation().getNome()+"| Motivo da solicitação: "+atual.getInformation().getMotivo());
+            atual = atual.getNextNode();
+        }
+    }
+
+    public boolean vazia(){
+        return primeiro == null;
+    }
 }
