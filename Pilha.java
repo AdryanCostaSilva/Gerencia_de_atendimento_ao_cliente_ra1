bbf968d338290d20b9e63f3f612bf77d2ce5d93b
Adryan Costa Silva <adryanbrabuu@gmail.com>
2025-10-16 17:25:53 -0300

    Finish Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,2 +1,35 @@
 public class Pilha {
+    private NodeSolicitacao topo;
+
+    public Pilha(){
+        this.topo = null;
+    }
+
+    public void inserir(ElementoSolicitacao elemento){
+       NodeSolicitacao novo = new NodeSolicitacao();
+       novo.setInformation(elemento);
+       novo.setNextNode(this.topo);
+       topo = novo;
+    }
+
+    public void remover(){
+        if (vazia()){
+            return;
+        }
+        topo = topo.getNextNode();
+    }
+
+    public void imprimir(){
+        int count = 0;
+        NodeSolicitacao atual = topo;
+        while (atual != null){
+            count++;
+            System.out.println("["+ count +"] Solicitação - ID: " + atual.getInformation().getId() + " | Descrição: " + atual.getInformation().getDescricao() + " | Data e Hora: " + atual.getInformation().getDataHora());
+            atual = atual.getNextNode();
+        }
+    }
+
+    public boolean vazia(){
+        return topo == null;
+    }
 }
