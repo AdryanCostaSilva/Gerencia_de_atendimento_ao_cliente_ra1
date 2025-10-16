3ca009f50af7f49a0165d024f711280264d1664f
Adryan Costa Silva <adryanbrabuu@gmail.com>
2025-10-16 10:49:52 -0300

    Finish Elemento.java

--- Elemento.java
+++ Elemento.java
@@ -1,2 +1,49 @@
 public class Elemento {
+    private String id;
+    private String descricao;
+    private String dataHora;
+    private String nome;
+    private String motivo;
+
+    public Elemento(){}
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
